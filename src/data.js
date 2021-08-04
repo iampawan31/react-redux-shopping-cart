178bf8b0ec0ce1ce520941d452705762e817283a
Pawan Kumar <surpawan@gmail.com>
2021-08-04 18:21:48 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    title: "Apple iPhone 12 (Blue, 128 GB)",
+    price: 82900.0,
+    img: "https://rukminim1.flixcart.com/image/832/832/kg8avm80/mobile/y/7/n/apple-iphone-12-dummyapplefsn-original-imafwg8dqq7z8cgh.jpeg?q=70",
+  },
+  {
+    id: 2,
+    title: "Samsung Galaxy A22 5G (Mint, 128 GB)  (6 GB RAM)",
+    price: 21499.0,
+    img: "https://rukminim1.flixcart.com/image/832/832/krme93k0/mobile/k/x/5/galaxy-a22-5g-sm-a226blghins-samsung-original-imag5dgzhyf8gjta.jpeg?q=70",
+  },
+  {
+    id: 3,
+    title: "Oppo A33 (Moonlight Black, 32 GB)  (3 GB RAM)",
+    price: 9900.0,
+    img: "https://rukminim1.flixcart.com/image/832/832/kgiaykw0/mobile/g/s/r/oppo-a33-cph2137-original-imafwqg8vkpgs265.jpeg?q=70",
+  },
+  {
+    id: 4,
+    title: "Realme C25s (Watery Grey, 128 GB)  (4 GB RAM)",
+    price: 11499.0,
+    img: "https://rukminim1.flixcart.com/image/832/832/kplisnk0/mobile/y/9/t/c25s-rmx3197-realme-original-imag3smcaztwgzeg.jpeg?q=70",
+  },
+];
